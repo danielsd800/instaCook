55961f7cc0770bb0bcffad4f2ca1e7da3e02b964
Daniel David <davidda@ws08m.local>
2017-01-25 13:06:29 +0200

    Initial Commit

---
+++ instacook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instacook
+//
+//  Created by Daniel David on 25/01/2017.
+//  Copyright © 2017 Daniel Yonit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
